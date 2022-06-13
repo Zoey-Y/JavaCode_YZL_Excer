82b66ebecabc20081ba614a11163df4fd844eb14
Zoey-Y <83832354+Zoey-Y@users.noreply.github.com>
2022-06-13 10:53:33 +0800

    Add files via upload

---
+++ EscapeCharacter.java
@@ -0,0 +1,24 @@
+public class EscapeCharacter{
+     public static void main(String[] args){
+       System.out.println("you're big liangzai gai");
+       System.out.println("you're big liangzai gai");
+       System.out.println("you're big liangzai gai");
+       System.out.println("you're big liangzai gai");
+       char a1 = 'a';
+       char a2 = 'a';
+       int a3 = 97;
+       char name01 = '杨';
+       System.out.println((int)name01+"\t 杨的Ascll码");
+       char name02 = 26472;
+       System.out.println(name02+"\t 26472的字");
+       System.out.println((char)a3 + "\t(char)a3");
+       System.out.println(a1+"char a1");
+       System.out.println((int)a2+"\t(int)a2");
+       System.out.println("you're big liangzai gai");
+
+       
+       System.out.println("you're big liangzai gai");
+       System.out.println("quick copy key:Shift + ctrl + D");
+
+            }
+} 
---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello{
+public static void main(String[] args){
+   int res = 1+2;
+   System.out.println("res ="+res);
+    }
+}

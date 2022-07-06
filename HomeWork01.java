c7746b4879d90fca139962c58660abcc6d954f68
Zoey-Y <83832354+Zoey-Y@users.noreply.github.com>
2022-07-06 11:19:33 +0800

    Add files via upload

--- HomeWork01.java
+++ HomeWork01.java
@@ -1,42 +1,90 @@
 public class HomeWork01{
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		/*
-		例题:-10.5 % 4
-		a % b当a为小数时 公式:a - (int)a / b * b
-		                    -10.5 - (int)(-10.5) / 4 * 4
+		目的:
+			1.某人有100,000元,没经过以此路口,需要交费,规则如下:
+			(1)当现金>50,000时,每次交5%
+			(2)当现金<=50,000时,每次交1000
+			编程计算该人可以经过多少次路口
+			要求:使用while break方式完成
 		*/
-		System.out.println(-10.5 % 4);//-2.5
-		/*-10.5 % 3.2
-			公式:a - (int)a /(int)b * b
-			    -10.5 - (-10) / 3 * (3.2) = -0.9
-		*/
-		System.out.println(-10.5 % 3.2);//-0.9
+		/*//标准答案:
+		double someone = 100000;//剩余钱数
+		int count = 0;//统计通过路口的次数
+		while(true){
+			if(someone > 50000){
+				someone *= 0.95;
+				count++;
+				System.out.println("----剩余钱数为:" + someone + "----次数为:" +count);
+			}else if(someone >= 1000){
+				someone -= 1000;
+				count++;
+				System.out.println("----剩余钱数为:" + someone + "----次数为:" + count);
+			}else{
+				
+				break;
+			}
 
-		/*注:小数位的这样的运算最终结果是其近似值*/
-
-		int i = 66;
-		System.out.println(++i+i);//i = i + 1 => i = 67(此时i重新被赋值了),其后i就是67,所以最后i + i ==67+67==134
-
-
-		/*
-		1.int num1 =(int)"18";//F:应该是Integer.parseInt("18");
-		2.int num2 =18.0;//F 因为int < double,double转int不能自动转换
-		3.double num3 = 3d;//T
-		4.double num4 = 8;//T因为int < double,int 转 double能自动转换
-		5.int i = 48;char ch = i+1;//F i+1为整型int,不能随便转换成char
-		6.byte b = 19;short s =b+2;//F b+2跟上面一个道理 
-		*/
-
-
-		/*String 转换 double 的语句*/
-		String words01 = "2.33";
-		double D01 = Double.parseDouble(words01);
-		System.out.println(D01);
+		}
+		System.out.println("100000的钱数可以路过" + count + "次路口" );*/
+		
 
 
-		/*char 转换 String 的语句*/
-		char c01 ='a';
-		String words02 = c01 + "";
-		System.out.println(words02);
+
+
+
+		/*//我的方法1:
+		double someone = 100000;//某人开始有的钱数
+		int stander = 50000;//收费准线
+		double charge = 0;//不同收费准线范围的收费钱数
+		int count = 0;//统计经过了多少次路口
+						while(someone > stander){
+							charge = someone*0.05;
+							someone -= charge;//某人的钱减少了
+							count++;
+							System.out.println("你的路费为:" + charge + "----剩余钱数为:" 
+												+ someone + "----次数为:" + count);
+							
+						}
+						
+						while(someone <= stander ){
+							charge = 1000;
+							someone -= charge;
+							count++;
+							System.out.println("你此时的路费为:" + charge + "----剩余钱数为:" 
+												+ someone + "----次数为:" + count);
+							if(someone < 1000){
+								System.out.println("已经没钱付路费了白白~");
+								break;
+							}
+							
+						}
+		System.out.println("你总共经过了" + count +"次路口");	*/	
+
+
+
+		
+		//我的方法2:	
+		int stander = 50000;
+			double charge = 0;
+			int count = 0;
+			for(double someone = 100000;someone > stander;someone -= charge){
+				charge = 100000*0.05;
+				count++;
+							System.out.println("你的路费为:" + charge + "----剩余钱数为:" 
+												+ someone + "----次数为:" +count);												
+			}
+			for(double someone = 100000;someone <= stander;someone -= charge){
+				charge = 1000;
+				count++;
+							System.out.println("你此时的路费为:" + charge + "----剩余钱数为:" 
+												+ someone + "----次数为:" + count);
+							if(someone < 1000){
+								System.out.println("已经没钱付路费了白白~");
+								break;
+							}
+			}
+		
+			
 	}
 }
--- HomeWork02.java
+++ HomeWork02.java
@@ -1,20 +1,23 @@
+import java.util.Scanner;
 public class HomeWork02{
-	public static void main(String[] args){
-		/*用char类型分别保存\n \t \r \\ 1 2 3这些字符,并打印输出*/
-		char c1 = '\n';
-		char c2 = '\t';
-		char c3 = '\r';
-		char c4 = '\\';
-		char c5 = '1';
-		char c6 = '2';
-		char c7 = '3';
-		System.out.println("用char类型分别保存'\\n' '\\t' '\\r' '\\\' '1' '2' '3'这些字符的结果为：");
-		System.out.println(c1); //换行
-		System.out.println(c2); //制表
-		System.out.println(c3); //回车
-		System.out.println(c4); //输出一个\
-		System.out.println(c5); //1
-		System.out.println(c6); //2
-		System.out.println(c7); //3
+	public static void main(String[] args) {
+		/*
+			目标:实现一个判断整数,属于哪个范围: >0 ; <0 ; =0
+			思路:
+				1.Scanner实现输入输出功能/或者利用(int)(Math.random)*100 + 1的方式来随机弄一个100以内的
+				2.判断范围
+		*/
+		Scanner input = new Scanner(System.in);
+		System.out.print("请随便输入一个整数>:");
+		int data = input.nextInt();//char word = input.next().At(0);
+			if(data > 0){
+				System.out.println("你输入的数大于0");
+			}else if(data < 0){
+				System.out.println("你输入的数小于0");
+			}else if(data == 0){
+				System.out.println("你输入的数等于0");
+			}else{
+				System.out.println("你输入的数不符合要求");
+			}
 	}
 }
